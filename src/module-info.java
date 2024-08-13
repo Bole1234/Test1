4466aca7a019c4d292bbfa90326b6361a2f4f933
datta <datta@dattachalwad>
2024-08-13 22:09:45 +0530

    commit new code

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author datta
+ *
+ */
+module project_Github {
+}
---
+++ src/sample/Test1.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Test1 {
+
+	public static void main(String[] args)
+	
+	
+	{
+		System.out.println("Welcome to Home");
+
+	}
+
+}
